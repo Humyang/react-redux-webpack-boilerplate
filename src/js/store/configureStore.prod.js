c9091980ec1fbca65a9f7b33b460b22b81e03d22
Yangxg <jessebutchman@gmail.com>
2016-06-30 02:55:30 +0800

    生产发模式添加 thunk middleware

--- src/js/store/configureStore.prod.js
+++ src/js/store/configureStore.prod.js
@@ -3,7 +3,14 @@ import rootReducer from '../reducer';
 
 // Middleware you want to use in production
 // const enhaner = applyMiddleware(p1,p2,p3);
-
+const enhancer = compose(
+    applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      loggerMiddleware // neat middleware that logs actions
+    )
+    // DevTools.instrument(),
+    // persistState(getDebugSessionKey())
+);
 export default function configureStore(initialState){
-    return createStore(rootReducer,initialState);
+    return createStore(rootReducer,initialState,enhancer);
 }
